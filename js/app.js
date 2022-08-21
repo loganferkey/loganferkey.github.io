d9179ce7e025fe8aed1ac168ed4ba79f47e7cf2c
Logan Ferkey <loganjferkey617@gmail.com>
2022-08-21 17:34:11 -0500

    Added flexbox styling to homepage

--- js/app.js
+++ js/app.js
@@ -12,3 +12,20 @@ tabs.forEach(tab => {
         target.classList.add('active');
     });
 });
+
+const commitTree = [
+    {
+        repo: 'website',
+        commithash: '140249fd90e09fd8s0d8f',
+        date: '08/21/2022'},
+    {
+        repo: 'website',
+        commithash: '203948fd0f83982098f8d9',
+        date: '05/15/2015'},
+    {
+        repo: 'website',
+        commithash: '1487fd9f93092809f89d8f9',
+        date: '04/28/2002'},
+];
+
+console.table(commitTree);
