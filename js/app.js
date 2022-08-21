8e8912c8e55298ee1cc014ee27e511482ed4d1d5
Logan Ferkey <loganjferkey617@gmail.com>
2022-08-21 15:38:26 -0500

    Added extra js classes on click

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@ const tabContents = document.querySelectorAll('[data-content-block]');
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget);
-        tabContents.forEach(tabContent => {tabContent.classList.remove('active');});
+        tabContents.forEach(tabContent => 
+            { tabContent.classList.remove('active'); });
+        tabs.forEach(t => { t.classList.remove('active-page'); });
+
+        tab.classList.toggle('active-page');
         target.classList.add('active');
     });
 });
